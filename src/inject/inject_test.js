e64a6b686dcbc3987b0d5b28e488a5b7f6fb7265
Kariuki <john.kariuki@andela.com>
2017-09-15 10:49:28 +0300

    chore: Log the correct plural state of the number of injected files (#193)

--- src/inject/inject_test.js
+++ src/inject/inject_test.js
@@ -581,7 +581,32 @@ describe('gulp-inject', function () {
 
     stream.on('end', function () {
       logOutput.should.have.length(1);
-      stripColor(logOutput[0]).should.equal('gulp-inject 1 files into template2.html.');
+      stripColor(logOutput[0]).should.equal('gulp-inject 1 file into template2.html.');
+      done();
+    });
+  });
+
+  it('should produce log output for multiple files actually injected (issue #192)', function (done) {
+    var logOutput = [];
+    gutil.log = function (a, b) {
+      logOutput.push(a + ' ' + b);
+    };
+
+    var target = src(['template2.html'], {read: true});
+    var sources = src([
+      'styles.css',
+      'app.css'
+    ]);
+
+    var stream = target.pipe(inject(sources));
+
+    // Dummy data reader to make the `end` event be triggered
+    stream.on('data', function () {
+    });
+
+    stream.on('end', function () {
+      logOutput.should.have.length(1);
+      stripColor(logOutput[0]).should.equal('gulp-inject 2 files into template2.html.');
       done();
     });
   });
